6ef99a6e9014d9bdadd7e726f246c4cbb6cd3a04
TvSpichkin <ericlen@mail.ru>
2024-06-01 22:48:10 +0300

    bolt & gaika

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,10 @@ app.get('/', (req: Request, res: Response) => {
     res.send('Hello Samurai!');
 });
 
+app.get('/prod', (req: Request, res: Response) => {
+    res.json([{id: 0, title: "bolt"}, {id: 1, title: "gaika"}]);
+});
+
 app.listen(port, () => {
     console.log("Server port: " + port);
 });
